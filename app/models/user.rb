96ea47e7dc64638bf702c41b1625aaa798c01ee7
YamamotoKou <nekou878@gmail.com>
2020-10-04 00:19:33 +0900

    userモデルpass設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,14 @@
 class User < ApplicationRecord
+  #[!]で直接emailの値を小文字にする．
+  before_save { email.downcase! }
+  validates :name,  presence: true, length: { maximum: 50 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: true
+  #ハッシュ化したパスワードを保存する．
+  #仮想的な属性password, password_confirmationを使えるように．
+  #authenticateメソッド, 一致するかどうかを調べるメソッド．
+  has_secure_password
+  validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_02_185822) do
+ActiveRecord::Schema.define(version: 2020_10_03_151218) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
   end
 
 end

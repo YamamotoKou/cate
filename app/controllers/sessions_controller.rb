d84fcff085e55ad2be914e91b6041ab0f96f838b
YamamotoKou <nekou878@gmail.com>
2020-10-05 01:48:11 +0900

    basic loginの作成

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,22 @@
+class SessionsController < ApplicationController
+
+  def new
+  end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user &.authenticate(params[:session][:password])
+      #user && user.authenticate(params[:session][:password])
+      log_in user
+      redirect_to user
+    else
+      flash.now[:danger] = '入力されたユーザー名やパスワードが正しくありません。確認してからやり直してください。'
+      render 'new'
+    end
+  end
+
+  def destroy
+    log_out
+    redirect_to root_url
+  end
+end

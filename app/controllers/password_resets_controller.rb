dbcfe17f3fc22eef96c233c7ecfc05c2bd811d20
YamamotoKou <nekou878@gmail.com>
2020-11-23 01:45:38 +0900

    パスワード再設定の作成

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,4 +1,7 @@
 class PasswordResetsController < ApplicationController
+  before_action :get_user,   only: [:edit, :update]
+  before_action :valid_user, only: [:edit, :update]
+  before_action :check_expiration, only: [:edit, :update]
 
   def new
   end
@@ -11,11 +14,58 @@ class PasswordResetsController < ApplicationController
       flash[:info] = "送信されたメールをご確認ください"
       redirect_to root_url
     else
-      flash.now[:danger] = "正しいメールアドレスを入力してください"
-      render 'new'
+      flash[:danger] = "正しいメールアドレスを入力してください"
+      redirect_to new_password_reset_path
     end
   end
 
   def edit
   end
+
+  def update
+    # 新しいパスワードがから文字列になっていないか
+    if params[:user][:password].empty?
+      @user.errors.add(:password, :blank)
+      render 'edit'
+    # 新しいパスワードが正しければ，更新する．
+    elsif @user.update(user_params)
+      log_in @user
+      # 再設定に成功したら，最後再設定されないようにする．
+      @user.update_attribute(:reset_digest, nil)
+      flash[:success] = "Password has been reset."
+      redirect_to @user
+    # 無効なパスワードがあれば失敗させる．
+    else
+      render 'edit'                                     # （2）への対応
+    end
+  end
+
+  private
+
+  # paramsハッシュ全体を初期化するという行為はセキュリティ上、極めて危険なので必要な属性のみ許可
+    def user_params
+      params.require(:user).permit(:password, :password_confirmation)
+    end
+
+    # フォームのemailでユーザーを特定する
+    def get_user
+      @user = User.find_by(email: params[:email])
+    end
+
+    # 正しいユーザーかどうか確認する
+    def valid_user
+      unless (@user && @user.activated? &&
+              @user.authenticated?(:reset, params[:id]))
+        redirect_to root_url
+      end
+    end
+
+    # トークンが期限切れかどうか確認する
+    def check_expiration
+      if @user.password_reset_expired?
+        flash[:danger] = "パスワード再設定の有効期限が切れています．"
+        redirect_to new_password_reset_url
+      end
+    end
+    
 end

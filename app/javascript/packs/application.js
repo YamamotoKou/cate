0db31eb6024108c0ecc9b492e9d2f623c3a2b869
YamamotoKou <nekou878@gmail.com>
2020-11-28 18:58:56 +0900

    無限スクロール機能の追加

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,7 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 require('jquery')
+require('jquery.jscroll.min.js')
 
 import "bootstrap"
 import "../stylesheets/application"

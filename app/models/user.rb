e48bc7316f1f213fd6c1318131465d903ad58ddb
YamamotoKou <nekou878@gmail.com>
2020-11-30 00:05:01 +0900

    _follow_form.html.erbの作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,7 +19,7 @@ class User < ApplicationRecord
   mount_uploader :avatar, ImageUploader
   #[!]で直接emailの値を小文字にする．
   before_save { email.downcase! }
-  validates :name,  presence: true, length: { maximum: 50 }
+  validates :name,  presence: true, length: { maximum: 50 }, uniqueness: true
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },
